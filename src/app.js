c9b4c603123db61a89edef1e187fe4164bea6732
thediggu <thediggu@gmail.com>
2017-11-14 21:51:05 +0530

    Basics WIP

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 import angular from 'angular';
+import common from './common/common';
 import sh from 'shelljs';
 
 angular.module('app', [
-
+  common
 ])
   .config(['$locationProvider', ($locationProvider) => {
     "ngInject";
@@ -11,12 +12,19 @@ angular.module('app', [
   .controller("myctrl", ($scope) => {
     'ngInject';
     $scope.name = "IONASDOIANSDOI";
+    sh.cd("C:/Users/dig14/Desktop");
     let items = sh.ls("-lA");
-    items = items.map(item => {
-      return {
-        name: item.name,
-        isDirectory: item.locked ? false : item.isDirectory()
+    $scope.items = items.map(item => {
+      try{
+        return {
+          name: item.name,
+          isDirectory: item.locked ? false : item.isDirectory()
+        }
+      } 
+      finally {
+        // do nothing
       }
+      
     });
     console.log(items)
   })
---
+++ src/common/common.js
@@ -0,0 +1,7 @@
+import './common.scss';
+
+let commonModule = angular.module('app.common', [
+])
+  .name
+
+export default commonModule;
